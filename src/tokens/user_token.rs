11ca64d32356595d5148f7a502bd4f2e92356ba4
Nathan Adams <dinnerbone@dinnerbone.com>
2020-12-11 19:00:27 +0100

    Add refresh ability to user tokens, and expose tokens for storage

--- src/tokens/user_token.rs
+++ src/tokens/user_token.rs
@@ -2,6 +2,7 @@ use crate::tokens::{
     errors::{RefreshTokenError, ValidationError},
     Scope, TwitchToken,
 };
+use crate::ClientSecret;
 use oauth2::{AccessToken, ClientId, RefreshToken};
 use oauth2::{HttpRequest, HttpResponse};
 use std::future::Future;
@@ -9,10 +10,13 @@ use std::future::Future;
 /// An User Token from the [OAuth implicit code flow](https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-implicit-code-flow) or [OAuth authorization code flow](https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-authorization-code-flow)
 #[derive(Debug, Clone)]
 pub struct UserToken {
-    access_token: AccessToken,
+    /// The access token used to authenticate requests with
+    pub access_token: AccessToken,
     client_id: ClientId,
+    client_secret: Option<ClientSecret>,
     login: Option<String>,
-    refresh_token: Option<RefreshToken>,
+    /// The refresh token used to extend the life of this user token
+    pub refresh_token: Option<RefreshToken>,
     expires: Option<std::time::Instant>,
     scopes: Vec<Scope>,
 }
@@ -23,13 +27,14 @@ impl UserToken {
         access_token: impl Into<AccessToken>,
         refresh_token: impl Into<Option<RefreshToken>>,
         client_id: impl Into<ClientId>,
+        client_secret: impl Into<Option<ClientSecret>>,
         login: Option<String>,
         scopes: Option<Vec<Scope>>,
-    ) -> UserToken
-    {
+    ) -> UserToken {
         UserToken {
             access_token: access_token.into(),
             client_id: client_id.into(),
+            client_secret: client_secret.into(),
             login,
             refresh_token: refresh_token.into(),
             expires: None,
@@ -42,6 +47,7 @@ impl UserToken {
         http_client: C,
         access_token: AccessToken,
         refresh_token: impl Into<Option<RefreshToken>>,
+        client_secret: impl Into<Option<ClientSecret>>,
     ) -> Result<UserToken, ValidationError<RE>>
     where
         RE: std::error::Error + Send + Sync + 'static,
@@ -53,6 +59,7 @@ impl UserToken {
             access_token,
             refresh_token.into(),
             validated.client_id,
+            client_secret,
             validated.login,
             validated.scopes,
         ))
@@ -61,21 +68,44 @@ impl UserToken {
 
 #[async_trait::async_trait(?Send)]
 impl TwitchToken for UserToken {
-    fn client_id(&self) -> &ClientId { &self.client_id }
+    fn client_id(&self) -> &ClientId {
+        &self.client_id
+    }
 
-    fn token(&self) -> &AccessToken { &self.access_token }
+    fn token(&self) -> &AccessToken {
+        &self.access_token
+    }
 
-    fn login(&self) -> Option<&str> { self.login.as_deref() }
+    fn login(&self) -> Option<&str> {
+        self.login.as_deref()
+    }
 
-    async fn refresh_token<RE, C, F>(&mut self, _: C) -> Result<(), RefreshTokenError<RE>>
+    async fn refresh_token<RE, C, F>(&mut self, http_client: C) -> Result<(), RefreshTokenError<RE>>
     where
         RE: std::error::Error + Send + Sync + 'static,
         C: FnOnce(HttpRequest) -> F,
-        F: Future<Output = Result<HttpResponse, RE>>, {
-        Err(RefreshTokenError::NoRefreshToken)
+        F: Future<Output = Result<HttpResponse, RE>>,
+    {
+        if let Some(client_secret) = self.client_secret.clone() {
+            let (access_token, expires, refresh_token) = if let Some(token) =
+                self.refresh_token.take()
+            {
+                crate::refresh_token(http_client, token, &self.client_id, &client_secret).await?
+            } else {
+                return Err(RefreshTokenError::NoRefreshToken);
+            };
+            self.access_token = access_token;
+            self.expires = expires;
+            self.refresh_token = refresh_token;
+        }
+        Ok(())
     }
 
-    fn expires(&self) -> Option<std::time::Instant> { None }
+    fn expires(&self) -> Option<std::time::Instant> {
+        None
+    }
 
-    fn scopes(&self) -> Option<&[Scope]> { Some(self.scopes.as_slice()) }
+    fn scopes(&self) -> Option<&[Scope]> {
+        Some(self.scopes.as_slice())
+    }
 }
