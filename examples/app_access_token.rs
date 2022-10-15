ecb8b174093fb77a79aeb0760c33f510a25798ee
Emil Gardström <emil.gardstrom@gmail.com>
2022-10-15 16:39:49 +0200

    don't request scopes for app access token
    this is not necessary, and probably never works

--- examples/app_access_token.rs
+++ examples/app_access_token.rs
@@ -4,9 +4,11 @@ use twitch_oauth2::TwitchToken;
 async fn main() -> anyhow::Result<()> {
     let _ = dotenv::dotenv(); // Eat error
     let mut args = std::env::args().skip(1);
+
     let reqwest = reqwest::Client::builder()
         .redirect(reqwest::redirect::Policy::none())
         .build()?;
+
     let client_id = std::env::var("TWITCH_CLIENT_ID")
         .ok()
         .or_else(|| args.next())
@@ -19,18 +21,12 @@ async fn main() -> anyhow::Result<()> {
         .map(twitch_oauth2::ClientSecret::new)
         .expect("Please set env: TWITCH_CLIENT_SECRET or pass client secret as an argument");
 
-    let scopes = std::env::var("CLIENT_SCOPES")
-        .ok()
-        .map(|s| s.split(' ').map(|s| s.to_string()).collect::<Vec<_>>())
-        .or_else(|| Some(args.collect::<Vec<_>>()))
-        .map(|v| v.into_iter().map(twitch_oauth2::Scope::from).collect())
-        .expect("Please set env: CLIENT_SCOPES or pass client secret as an argument");
-
+    // Get the app access token
     let token = twitch_oauth2::AppAccessToken::get_app_access_token(
         &reqwest,
         client_id,
         client_secret,
-        scopes,
+        vec![],
     )
     .await?;
 
