42431aa70d1f807a9e2059401b2edc2ae6611f2b
Emil Gardström <emil.gardstrom@gmail.com>
2023-01-24 23:39:33 +0100

    bump deps

--- src/types.rs
+++ src/types.rs
@@ -3,6 +3,8 @@
 
 use std::fmt;
 
+use base64::Engine;
+
 /// A Client Id
 #[aliri_braid::braid(serde)]
 pub struct ClientId;
@@ -75,7 +77,7 @@ impl CsrfToken {
     pub fn new_random_len(len: u32) -> CsrfToken {
         use rand::Rng as _;
         let random_bytes: Vec<u8> = (0..len).map(|_| rand::thread_rng().gen::<u8>()).collect();
-        CsrfToken::new(base64::encode_config(random_bytes, base64::URL_SAFE_NO_PAD))
+        CsrfToken::new(base64::engine::general_purpose::STANDARD.encode(random_bytes))
     }
 }
 
